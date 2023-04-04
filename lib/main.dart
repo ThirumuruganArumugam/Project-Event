b47a6281fd34fcfd7217b349d366f091d16e5967
Thirumurugan Arumugam <Thirumurugan.A@avasoft.com>
2023-04-04 18:18:13 +0530

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'App.dart';
 /* Create a main method */
 void main() {
+  
   /* Invoke a method named runapp() and assign constructor as the parameter */
   runApp(const Myapp());
 }
